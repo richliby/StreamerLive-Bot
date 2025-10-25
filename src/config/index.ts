55da8eee0746fd71935c02c87030ff5d1b4a6780
Greg <gbrewton1980@gmail.com>
2025-10-25 13:55:55 -0600

    Move configuration settings to a new index.ts file

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+export const roles: Record<number, string> = {
+    5: '123456789012345678',
+    10: '223456789012345678',
+    20: '323456789012345678',
+    30: '423456789012345678',
+    50: '523456789012345678'
+}
+
+export const appConfig: Record<string, string> = {
+    ownerId: "Dverge",
+    cmdPrefix: "!"
+}
